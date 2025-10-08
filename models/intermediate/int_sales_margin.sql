0f28c4c595b47baf9b71093f7e37acd88e3aceed
ozkncaonur@gmail.com <ozkncaonur@gmail.com>
2025-10-08 14:09:49 +0000

    Added int_sales_margin

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,39 @@
+-- models/intermediate/int_sales_margin.sql
+
+with
+sales as (
+    select *
+    from {{ ref('stg_sales') }}
+),
+
+products as (
+    select *
+    from {{ ref('stg_product') }}
+),
+
+joined as (
+    select
+        s.orders_id,
+        s.product_id,
+        s.quantity,
+        s.revenue,
+        p.purchase_price
+    from sales s
+    left join products p
+        on s.product_id = p.product_id
+),
+
+calculated as (
+    select
+        orders_id,
+        product_id,
+        quantity,
+        revenue,
+        purchase_price,
+        quantity * purchase_price as purchase_cost,
+        revenue - (quantity * purchase_price) as margin
+    from joined
+)
+
+select *
+from calculated
