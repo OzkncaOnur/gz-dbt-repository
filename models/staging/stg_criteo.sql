984d5285ae04b8ed6536456a883277be6021926c
Onur Ozkanca <ozkncaonur@gmail.com>
2025-10-09 13:28:28 +0000

    Added models for the adwords/bings/criteo/facebook

---
+++ models/staging/stg_criteo.sql
@@ -0,0 +1,10 @@
+
+select
+    cast(date_date as date) as date_date,                     -- Kampanya verisinin kayıt tarihi
+    cast(paid_source as STRING) as paid_source,              -- Pazarlama kanalı (örneğin 'google_ads')
+    cast(campaign_key as STRING) as campaign_id,              -- Kampanya kimliği
+    cast(camPGN_name as STRING) as campaign_name,            -- Kampanya adı (camPGN_name'den değiştirildi)
+    cast(ads_cost as float64) as ads_cost,                       -- Reklam maliyeti, string'den float'a dönüştürüldü
+    cast(impression as numeric) as impression,                 -- Gösterim sayısı
+    cast(click as numeric) as click                            -- Reklam tıklama sayısı
+from {{ source('gz_raw_data', 'raw_gz_criteo') }}
